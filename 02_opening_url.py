2f836632c337e08ca3330906998ecb64384cebaa
Prapti49 <prapti.khatun@doer.com.bd>
2025-07-20 20:24:46 +0600

    back

--- 02_opening_url.py
+++ 02_opening_url.py
@@ -1,8 +1,19 @@
+import time
 from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get("https://google.com")
 driver.maximize_window()
+driver.get("https://google.com")
+time.sleep(3)
+driver.get("https://apple.com")
+time.sleep(3)
 
+driver.back()
+time.sleep(3)
 
+driver.forward()
+time.sleep(3)
+
+driver.refresh()
 # driver.close()
-# driver.quit()
+driver.quit()
