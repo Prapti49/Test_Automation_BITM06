96e8b6f5af2fb402581d7c8a480cc7c571fc3f49
Prapti49 <prapti.khatun@doer.com.bd>
2025-07-22 19:39:42 +0600

    waits

--- Selenium_Basics/05_waits.py
+++ Selenium_Basics/05_waits.py
@@ -2,6 +2,9 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.implicitly_wait(5)
@@ -14,5 +17,10 @@ try:
     bangladeshi_no.click()
 except Exception as e:
     print("Element 'No' not found with implicit wait.")
-bangladeshi_yes = driver.find_element(By.CSS_SELECTOR, "label[for='applying-bangladeshCommon.Labels.Yes']")
-bangladeshi_yes.click()
+
+try:
+    wait = WebDriverWait(driver, 20)
+    bangladeshi_yes = wait.until(EC.presence_of_element_located((By.css_selector, "label[for='applying-bangladeshCommon.Labels.Yes']")))
+    bangladeshi_yes.click()
+except Exception as e:
+    print("Element 'Yes' not found with Explicit wait.")
